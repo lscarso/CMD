6406e12b08134e6c7378514760eaedd5ae36c9de
Scarsini <luca.scarsini@kiongroup.com>
2016-07-07 11:43:03 +0200

    Correct missing pipe

--- Set-FirefoxPref/Invoke_FirefoxAutoconfig.cmd
+++ Set-FirefoxPref/Invoke_FirefoxAutoconfig.cmd
@@ -1,6 +1,6 @@
 REM Create Mozilla.cfg
 if exist "%ProgramFiles(x86)%\Mozilla Firefox\" ECHO lockPref("autoadmin.global_config_url","http://ITLTEAPP01.d400.mh.grp/autoadmin.js");>> "%ProgramFiles(x86)%\Mozilla Firefox\mozilla.cfg"
-if exist "%ProgramFiles(x86)%\Mozilla Firefox\" ECHO lockPref("autoadmin.append_emailaddr",false); "%ProgramFiles(x86)%\Mozilla Firefox\mozilla.cfg"
+if exist "%ProgramFiles(x86)%\Mozilla Firefox\" ECHO lockPref("autoadmin.append_emailaddr",false);>> "%ProgramFiles(x86)%\Mozilla Firefox\mozilla.cfg"
 
 REM Create it8-kion_autoconfig.js
 if not exist "%ProgramFiles(x86)%\Mozilla Firefox\defaults\preferences" MKDIR "%ProgramFiles(x86)%\Mozilla Firefox\defaults\preferences" 
