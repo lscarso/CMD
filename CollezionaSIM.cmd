1c3066722f49140f5466d5d0d132effce6585ea9
Scarsini <luca.scarsini@kiongroup.com>
2020-11-09 16:23:50 +0100

    Add another regkey

--- CollezionaSIM.cmd
+++ CollezionaSIM.cmd
@@ -4,8 +4,11 @@
 :: .EXAMPLE
 :: CollectICCID.cmd
 
-for /F "tokens=*" %%G in ('reg QUERY "HKCU\Software\Sierra Wireless Inc\Skylight\DataUsage" /f 89????????????????? /k 2^>NUL ^| FINDSTR /R /C:"HKEY_"') do (set VAR=%%G)
+# Skylight
+#for /F "tokens=*" %%G in ('reg QUERY "HKCU\Software\Sierra Wireless Inc\Skylight\DataUsage" /f 89????????????????? /k 2^>NUL ^| FINDSTR /R /C:"HKEY_"') do (set VAR=%%G)
+#SET "VAR=%VAR:~66%"
 
-SET "VAR=%VAR:~66%"
+for /F "tokens=*" %%G in ('reg query "hklm\SOFTWARE\Microsoft\Multivariant\UICC" /f 89????????????????? /k 2^>NUL ^| FINDSTR /R /C:"HKEY_"') do (set VAR=%%G)
+SET "VAR=%VAR:~56%"
 
 echo %VAR%,%COMPUTERNAME%,%USERNAME% >> \\SP8IT000001.d400.mh.grp\Burning\Luca\Script\NumSim.txt
