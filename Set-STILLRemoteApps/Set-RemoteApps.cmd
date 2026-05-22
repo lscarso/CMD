566efa0415cc740a47f8e7f860812b4276d56066
lscarso <lscarso@users.noreply.github.com>
2026-05-22 10:22:27 +0200

    Delete Set-STILLRemoteApps directory

--- Set-STILLRemoteApps/Set-RemoteApps.cmd
+++
@@ -1,6 +0,0 @@
-ECHO OFF
-:: NAME: SET-REMOTEAPPS.CMD v1.0
-:: DATE: 31/08/2016
-:: PURPOSE:  Deploy RemoteApps on Windows 7 - Starting from 8, it's possible to use GPO
-
-rundll32.exe tsworkspace,WorkspaceSilentSetup \\SP8IT000001.d400.mh.grp\Burning\STILL-RemoteApps.wcx
